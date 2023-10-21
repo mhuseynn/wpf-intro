1cdd8105169b4a0ca8bee4670fca67d8c07774f0
mhuseynn <hmehdiyev2005@gmail.com>
2023-10-21 17:41:39 +0400

    first

---
+++ wpf-practics/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_practics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Button btn = sender as Button;
+
+            Random random = new Random();
+            byte r = (byte)random.Next(256);
+            byte g = (byte)random.Next(256);
+            byte b = (byte)random.Next(256);
+            Color color = Color.FromRgb(r, g, b);
+            btn.Background = new SolidColorBrush(color);
+            MessageBox.Show($"{btn.Content} color changed");
+
+        }
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            grid.Children.Remove(btn);
+            this.Title = btn.Content.ToString();
+        }
+    }
+}
